534d2b76ba81fd9a36dac76a0299716489e3b10a
19015428 <19015428@LAPTOP-734V9TF0>
2020-07-23 17:08:38 +0800

    Changed returnChromebook

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -281,11 +281,30 @@ public class ResourceCentre {
 	public static boolean doReturnChromebook(ArrayList<Chromebook> chromebookList,String tag){
 		boolean isReturned = false;
 		// write your code here
+		for (int i = 0; i < chromebookList.size(); i++) {
+			if (tag.equalsIgnoreCase(chromebookList.get(i).getAssetTag()) && chromebookList.get(i).getIsAvailable() == false) {
+				
+			}
+			chromebookList.get(i).setIsAvailable(true);
+			chromebookList.get(i).setDueDate("");
+		}
+		
 		return isReturned;
 	}
 	public static void returnChromebook(ArrayList<Chromebook> chromebookList) {
 		// write your code here
+		ResourceCentre.viewAllChromebook(chromebookList);
+		String tag = Helper.readString("enter asset tag >");
+		Boolean isReturned = doReturnChromebook(chromebookList, tag);
 		// write your code here
+		if (isReturned == false) {
+			System.out.println("Invalid");
+			
+		} 
+		else {
+			System.out.println("Chromebook" + tag + "returned");
+
+		}
 	}
 
 
