d2f648c4dfc50a0c5527eddabf5675942ec12203
19017935 <19017935@LAPTOP-AQ7K0M9Q>
2020-07-23 14:33:01 +0800

    changed inputChromebook and addChromebook. Soon Chuan

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -173,11 +173,20 @@ public class ResourceCentre {
 	public static Chromebook inputChromebook() {	
 		Chromebook cb =null;
 		// write your code here
+		String tag = Helper.readString("Enter asset tag > ");
+		String description = Helper.readString("Enter description > ");
+		String operatingSys = Helper.readString("Enter Operating System > ");
+		
+		cb = new Chromebook(tag, description, operatingSys);
 		return cb;
 		
 	}	
 	public static void addChromebook(ArrayList<Chromebook> chromebookList, Chromebook cb) {
 		// write your code here
+		
+		chromebookList.add(cb);
+		System.out.println("Chromebook added");
+		
 	}
 	
 	//================================= Option 3 Loan =================================
